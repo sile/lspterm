079d110d58a92f4649de15066bfbcf874f84d979
Takeru Ohta <phjgt308@gmail.com>
2025-07-22 00:08:38 +0900

    Add terminal initialization to App struct

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,21 @@
+use orfail::OrFail;
+use tuinix::Terminal;
+
 use crate::lsp_client::LspClient;
 
 #[derive(Debug)]
 pub struct App {
+    terminal: Terminal,
     lsp_client: LspClient,
 }
 
 impl App {
     pub fn new(lsp_client: LspClient) -> orfail::Result<Self> {
-        Ok(Self { lsp_client })
+        let terminal = Terminal::new().or_fail()?;
+        Ok(Self {
+            terminal,
+            lsp_client,
+        })
     }
 
     pub fn run(self) -> orfail::Result<()> {
