7a8234381cc4abf9d1fd3dfc801d58ae568b7866
Takeru Ohta <phjgt308@gmail.com>
2025-07-28 08:58:32 +0900

    Refactor LSP initialization to standalone functions and add directional logging prefixes

--- src/lsp_server.rs
+++ src/lsp_server.rs
@@ -88,8 +88,7 @@ impl LspServer {
         let mut stdout = BufReader::new(process.stdout.take().or_fail()?);
 
         // Initialize the LSP server
-        Self::initialize_lsp_server(&spec, workspace_folder_uri, &mut stdout, &mut stdin)
-            .or_fail()?;
+        initialize_lsp_server(&spec, workspace_folder_uri, &mut stdout, &mut stdin).or_fail()?;
 
         let (message_tx, message_rx) = std::sync::mpsc::channel();
         let message_tx_for_stdout = message_tx.clone();
@@ -158,44 +157,6 @@ impl LspServer {
         Ok(())
     }
 
-    fn initialize_lsp_server<R, W>(
-        spec: &LspServerSpec,
-        workspace_folder_uri: DocumentUri,
-        mut reader: R,
-        mut writer: W,
-    ) -> orfail::Result<()>
-    where
-        R: BufRead,
-        W: Write,
-    {
-        let params = nojson::object(|f| {
-            f.member("clientInfo", Self::client_info())?;
-            f.member(
-                "workspaceFolders",
-                [Self::workspace_folder(&workspace_folder_uri)],
-            )?;
-            f.member(
-                "capabilities",
-                nojson::RawJson::parse(include_str!("capabilities.json")).expect("bug"),
-            )?;
-            if let Some(options) = &spec.initialize_options {
-                f.member("initializationOptions", options)?;
-            }
-            Ok(())
-        });
-        let json = lsp::send_request(&mut writer, INITIALIZE_REQUEST_ID, "initialize", params)
-            .or_fail()?;
-        println!("{json}");
-
-        let json = lsp::recv_message(&mut reader).or_fail()?.or_fail()?;
-        println!("{json}");
-
-        let json = lsp::send_notification(&mut writer, "initialized", ()).or_fail()?;
-        println!("{json}");
-
-        Ok(())
-    }
-
     fn run_stdin_loop(
         mut stdin: ChildStdin,
         message_rx: Receiver<LspMessage>,
@@ -212,13 +173,13 @@ impl LspServer {
                 } => {
                     let json = lsp::send_request(&mut stdin, next_request_id, &method, params)
                         .or_fail()?;
-                    println!("{json}");
+                    println!("--> {json}");
                     ongoing_requests.insert(next_request_id, reply_tx);
                     next_request_id += 1;
                 }
                 LspMessage::Notification { method, params } => {
                     let json = lsp::send_notification(&mut stdin, &method, params).or_fail()?;
-                    println!("{json}");
+                    println!("--> {json}");
                 }
                 LspMessage::ResponseFromLspServer { request_id, result } => {
                     if let Some(reply_tx) = ongoing_requests.remove(&request_id) {
@@ -227,7 +188,7 @@ impl LspServer {
                 }
                 LspMessage::ResponseToLspServer { request_id, result } => {
                     let json = lsp::send_response(&mut stdin, request_id, result).or_fail()?;
-                    println!("{json}");
+                    println!("--> {json}");
                 }
                 LspMessage::LspServerStdoutError => break,
             }
@@ -240,7 +201,7 @@ impl LspServer {
         message_tx: Sender<LspMessage>,
     ) -> orfail::Result<()> {
         while let Some(json) = lsp::recv_message(&mut stdout).or_fail()? {
-            println!("{json}");
+            println!("<-- {json}");
 
             let value = json.value();
             let Some(request_id) = value.to_member("id").or_fail()?.get() else {
@@ -280,18 +241,56 @@ impl LspServer {
         }
         Ok(())
     }
+}
 
-    fn client_info() -> impl nojson::DisplayJson {
-        nojson::object(|f| {
-            f.member("name", env!("CARGO_PKG_NAME"))?;
-            f.member("version", env!("CARGO_PKG_VERSION"))
-        })
-    }
+fn initialize_lsp_server<R, W>(
+    spec: &LspServerSpec,
+    workspace_folder_uri: DocumentUri,
+    mut reader: R,
+    mut writer: W,
+) -> orfail::Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    let params = nojson::object(|f| {
+        f.member("clientInfo", client_info())?;
+        f.member(
+            "workspaceFolders",
+            [workspace_folder(&workspace_folder_uri)],
+        )?;
+        f.member(
+            "capabilities",
+            nojson::RawJson::parse(include_str!("capabilities.json")).expect("bug"),
+        )?;
+        if let Some(options) = &spec.initialize_options {
+            f.member("initializationOptions", options)?;
+        }
+        Ok(())
+    });
+    let json =
+        lsp::send_request(&mut writer, INITIALIZE_REQUEST_ID, "initialize", params).or_fail()?;
+    println!("--> {json}");
 
-    fn workspace_folder(workspace_folder_uri: &DocumentUri) -> impl nojson::DisplayJson + '_ {
-        nojson::object(move |f| {
-            f.member("uri", workspace_folder_uri)?;
-            f.member("name", "main")
-        })
-    }
+    let json = lsp::recv_message(&mut reader).or_fail()?.or_fail()?;
+    println!("<-- {json}");
+
+    let json = lsp::send_notification(&mut writer, "initialized", ()).or_fail()?;
+    println!("--> {json}");
+
+    Ok(())
+}
+
+fn client_info() -> impl nojson::DisplayJson {
+    nojson::object(|f| {
+        f.member("name", env!("CARGO_PKG_NAME"))?;
+        f.member("version", env!("CARGO_PKG_VERSION"))
+    })
+}
+
+fn workspace_folder(workspace_folder_uri: &DocumentUri) -> impl nojson::DisplayJson + '_ {
+    nojson::object(move |f| {
+        f.member("uri", workspace_folder_uri)?;
+        f.member("name", "main")
+    })
 }
